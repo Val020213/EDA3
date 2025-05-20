1f6cbf1ad686ba2f1f9447e1c03c4b548a78915d
osvaldo.rmp <osvaldo.rmp@avangenio.com>
2025-05-20 03:10:24 -0400

    add competitive companion

---
+++ problems/hash/E_Compress_Words.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+
+const int INF = 2e9,
+          MOD = 1e9 + 7,
+          INFLL = 1e18;
+
+void solve()
+{
+    // code
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+}
